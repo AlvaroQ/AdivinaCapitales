580fdf4f606354a9f6f1ca895a762016047e942a
Quintana <alvaroqp@gmail.com>
2022-04-03 11:19:23 +0200

    button style on select screen

--- app/src/main/java/com/alvaroquintana/adivinacapitales/ui/select/SelectFragment.kt
+++ app/src/main/java/com/alvaroquintana/adivinacapitales/ui/select/SelectFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import com.alvaroquintana.adivinacapitales.R
 import com.alvaroquintana.adivinacapitales.common.startActivity
@@ -28,12 +29,12 @@ class SelectFragment : Fragment() {
         binding = SelectFragmentBinding.inflate(inflater)
         val root = binding.root
 
-        val btnStartByFlag: Button = root.findViewById(R.id.btnStartByFlag)
+        val btnStartByFlag: CardView = root.findViewById(R.id.btnStartByFlag)
         btnStartByFlag.setSafeOnClickListener {
             selectViewModel.navigateToGameByFlag()
         }
 
-        val btnStartByCountry: Button = root.findViewById(R.id.btnStartByCountry)
+        val btnStartByCountry: CardView = root.findViewById(R.id.btnStartByCountry)
         btnStartByCountry.setSafeOnClickListener {
             selectViewModel.navigateToGameByCountry()
         }
